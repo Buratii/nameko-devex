19296e0e28a8a02905d3aac769e094ee37bba509
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-16 22:00:49 -0300

    feat: create selection button component

---
+++ gateapi/web/src/lib/components/selectionButton/SelectionButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let name: string;
+  export let icon: string;
+</script>
+
+<button 
+  class="flex flex-row items-center justify-center w-48.5 my-2 py-5 rounded border border-purple-200 focus:border-purple-500 focus:text-purple-500 focus:bg-purple-100"
+  >
+  <img class="text-purple-500" src={icon} alt={name}>
+  <p class="ml-3.5 font-bold">{name}</p>
+</button>
