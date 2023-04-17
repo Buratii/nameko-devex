798b80cf6c6af43583eba0f85852782ccb1699f5
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-16 22:00:29 -0300

    feat: create input component

---
+++ gateapi/web/src/lib/components/input/Input.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { HTMLInputAttributes } from 'svelte/elements'
+
+  interface $$Props extends HTMLInputAttributes {
+    label: string
+  }
+
+  export let label: string;
+</script>
+
+<div class="flex flex-col">
+  <span class="font-bold">{label}</span>
+  <input class=" h-12 border border-purple-200 rounded focus:outline-none text-base px-3" type="text" {...$$restProps} />
+</div>
