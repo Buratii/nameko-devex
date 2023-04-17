0b457734338dbcc45aa2dfa58ea796da9a8f654e
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-16 22:00:07 -0300

    feat: create theme and lint

--- gateapi/web/tailwind.config.cjs
+++ gateapi/web/tailwind.config.cjs
@@ -1,7 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.svelte'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple': {
+          100: '#F3F1FE',
+          200: '#C3C1D7',
+          400: '#745CF1',
+          500: '#3F25C8',
+          800: '#35344C',
+        }
+      },
+      spacing: {
+        '48.5': '194px',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
