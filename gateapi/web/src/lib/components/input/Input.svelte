6a6f41fb21eb6e42e742d31194b4ea8b67edb7df
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-18 15:26:27 -0300

    feat: add new styles to Input

--- gateapi/web/src/lib/components/input/Input.svelte
+++ gateapi/web/src/lib/components/input/Input.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
   import type { HTMLInputAttributes } from 'svelte/elements'
+  import Icon from '../icon/Icon.svelte'
 
   interface $$Props extends HTMLInputAttributes {
     label: string
+    icon?: string;
+    suffix?: string;
   }
 
   export let label: string;
+  export let icon: string = null;
+  export let suffix = null;
 </script>
 
-<div class="flex flex-col">
-  <span class="font-bold">{label}</span>
-  <input class=" h-12 border border-purple-200 rounded focus:outline-none text-base px-3" type="text" {...$$restProps} />
+<div class=" w-full box-content flex flex-col">
+  <span class="font-medium mb-0.5">{label}</span>
+  <div class="h-12 flex items-center justify-center px-3 py-2 border focus-within:border-purple-500 border-purple-200 rounded focus:ease-linear duration-100">
+    {#if icon}
+      <Icon class="h-full fill-purple-700" name={icon} />
+    {/if}
+    <input class={` w-full focus:outline-none text-base  placeholder:color-purple-200`} {...$$restProps} />
+
+    {#if suffix}
+    <p class="pl-1 text-sm color-purple-700">{suffix}</p>
+    {/if}
+  </div>
 </div>
