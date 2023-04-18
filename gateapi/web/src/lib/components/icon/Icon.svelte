5176481f6b4859a8266524dfc001ca3f7703a3d7
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-18 15:26:04 -0300

    feat: create icon component

---
+++ gateapi/web/src/lib/components/icon/Icon.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  import pathsByName from "./icon-paths"
+</script>
+
+<script lang="ts">
+  export let name
+
+  $: paths = pathsByName[name].paths || []
+  $: viewBox = pathsByName[name].viewBox || ""
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  class={`fill-current ${$$props.class}`}
+  viewBox={viewBox}>
+  {#each paths as path}
+    <path d="{path}"></path>
+  {/each}
+</svg>
