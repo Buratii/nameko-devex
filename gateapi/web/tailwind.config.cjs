48ab7229917bfcff92fb5ec1a240239033a78bc1
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-18 15:24:46 -0300

    feat: add new themes to tailwind

--- gateapi/web/tailwind.config.cjs
+++ gateapi/web/tailwind.config.cjs
@@ -1,24 +1,37 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.svelte'],
+  content: ['./src/**/*.html', './src/**/*{.svelte,.svg}', './src/lib/assets/svgs/*.svg'],
+  // content: ["./src/*/.{astro,html,js,jsx,svelte,ts,tsx,vue,svg}"],
   theme: {
     extend: {
+      boxShadow: {
+        'md': '0px 4px 12px rgba(0, 0, 0, 0.1)',
+      },
+      dropShadow: {
+        'md': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      },
       colors: {
+        'gray': {
+          50: '#E5E5E4',
+        },
         'purple': {
           100: '#F3F1FE',
           200: '#C3C1D7',
           400: '#745CF1',
           500: '#3F25C8',
+          700: '#474374',
           800: '#35344C',
         }
       },
       spacing: {
-        '48.5': '194px',
+        '10.5': '2.625rem',
+        '18': '4.25rem',
+        '48.5': '12.125rem',
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif']
-      }
+      },
     },
   },
   plugins: [],
