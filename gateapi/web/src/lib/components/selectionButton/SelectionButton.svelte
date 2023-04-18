aea57650b2b8701f59b5f3f41877641d17f2dbd0
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-18 15:27:34 -0300

    feat: update styles from selection button

--- gateapi/web/src/lib/components/selectionButton/SelectionButton.svelte
+++ gateapi/web/src/lib/components/selectionButton/SelectionButton.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+  import Icon from "../icon/Icon.svelte"
+
   export let name: string;
   export let icon: string;
 </script>
 
 <button 
-  class="flex flex-row items-center justify-center w-48.5 my-2 py-5 rounded border border-purple-200 focus:border-purple-500 focus:text-purple-500 focus:bg-purple-100"
+  class="flex flex-row items-center justify-center w-48.5 my-2 py-5 rounded border border-purple-200 focus:border-purple-500 focus:text-purple-500 focus:bg-purple-100 focus:ease-linear duration-100"
   >
-  <img class="text-purple-500" src={icon} alt={name}>
-  <p class="ml-3.5 font-bold">{name}</p>
+    <Icon class="w-8 h-8 focus:fill-purple-500" name={icon} />
+    <p class="ml-3.5 font-bold">{name}</p>
 </button>
