dfdfe0ba606c3b05a2d42acfaa17d0d2a5178954
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-19 20:17:49 -0300

    feat: update section styles

--- gateapi/web/src/lib/components/section/Section.svelte
+++ gateapi/web/src/lib/components/section/Section.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  export let label: string;
+	export let label: string
 </script>
 
 <div class={`${$$props.class} w-full mt-6`}>
-  <span class="font-bold">{label}</span>
+	<span class="font-bold">{label}</span>
 
-  <div class="section flex flex-row justify-between pt-2">
-    <slot></slot>
-  </div>
+	<div class="section flex flex-row justify-between pt-2">
+		<slot />
+	</div>
 </div>
