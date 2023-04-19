dd9b053d4e2d7b6101c0531ebf984fd068c12d3c
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-19 20:18:08 -0300

    feat: update selection button types and styles

--- gateapi/web/src/lib/components/selectionButton/SelectionButton.svelte
+++ gateapi/web/src/lib/components/selectionButton/SelectionButton.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-  import Icon from "../icon/Icon.svelte"
+	import Icon from '../icon/Icon.svelte'
+	import type pathsByName from '../icon/icon-paths'
 
-  export let name: string;
-  export let icon: string;
+	export let name: string
+	export let icon: keyof typeof pathsByName
 </script>
 
-<button 
-  class="flex flex-row items-center justify-center w-48.5 my-2 py-5 rounded border border-purple-200 focus:border-purple-500 focus:text-purple-500 focus:bg-purple-100 focus:ease-linear duration-100"
-  >
-    <Icon class="w-8 h-8 focus:fill-purple-500" name={icon} />
-    <p class="ml-3.5 font-bold">{name}</p>
+<button
+	class="flex flex-row items-center justify-center w-48.5 my-2 py-5 rounded border border-purple-200 focus:border-purple-500 focus:text-purple-500 focus:bg-purple-100 focus:ease-linear duration-100"
+>
+	<Icon class="w-8 h-8 focus:fill-purple-500" name={icon} />
+	<p class="ml-3.5 font-bold">{name}</p>
 </button>
