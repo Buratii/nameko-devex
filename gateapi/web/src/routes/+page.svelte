8d085507d208a6fdc45feae0a09a3f35a26bdc96
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-19 20:20:02 -0300

    feat: update home styles

--- gateapi/web/src/routes/+page.svelte
+++ gateapi/web/src/routes/+page.svelte
@@ -1,13 +1,18 @@
 <script context="module" lang="ts">
 	import { ROUTES } from './routes'
+
+	console.log(ROUTES)
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<main>
+<main class="h-fit w-full px-8 py-4 shadow-sm">
 	{#each Object.entries(ROUTES) as [name, route]}
-		<a class="mx-2 text-blue-400 underline" href={route}>{name}</a>
+		<a
+			class="mx-2 text-blue-40 font-amplitude text-2xl text-purple-800 hover:text-purple-400 hover:ease-linear duration-100"
+			href={route}>{name}</a
+		>
 	{/each}
 </main>
