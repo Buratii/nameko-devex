f427f689eef11432185734344ee27acff7607c77
Leonardo Burati <leonardoburati@hotmail.com>
2023-04-19 20:16:53 -0300

    feat: update icon paths and types

--- gateapi/web/src/lib/components/icon/Icon.svelte
+++ gateapi/web/src/lib/components/icon/Icon.svelte
@@ -1,19 +1,20 @@
 <script context="module">
-  import pathsByName from "./icon-paths"
+	import pathsByName from './icon-paths'
 </script>
 
 <script lang="ts">
-  export let name
+	export let name: keyof typeof pathsByName
 
-  $: paths = pathsByName[name].paths || []
-  $: viewBox = pathsByName[name].viewBox || ""
+	$: paths = pathsByName[name].paths || []
+	$: viewBox = pathsByName[name].viewBox || ''
 </script>
 
 <svg
-  xmlns="http://www.w3.org/2000/svg"
-  class={`fill-current ${$$props.class}`}
-  viewBox={viewBox}>
-  {#each paths as path}
-    <path d="{path}"></path>
-  {/each}
+	xmlns="http://www.w3.org/2000/svg"
+	class={`fill-current ${$$props.class}`}
+	{viewBox}
+>
+	{#each paths as path}
+		<path d={path} />
+	{/each}
 </svg>
